f2faeb9ae787e49409d6ef65029443efe748af92
zu-min <okka.0928@gmail.com>
2020-03-30 16:17:13 +0900

    basic

---
+++ basic/basic.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* This is my first sample program. */
+	fmt.Println("Hello, World!")
+	/* my first program in Go */
+	fmt.Println("I am in Go Programming World!")
+}
